16245b44b6b95ca1316e208e4a3f8f3d51952724
Derusu <vladislaav5@gmail.com>
2024-04-27 00:22:13 +0300

    modified:   index.html 	new file:   js/gsap.min.js 	new file:   js/gsap.min.js.map 	new file:   js/header.js 	modified:   style.css 	modified:   style.css.map 	modified:   style.scss

---
+++ js/header.js
@@ -0,0 +1,90 @@
+class Menu {
+    constructor (obj) {
+        this.button = document.querySelector(obj.button);
+        this.screen = document.querySelector(obj.screen);
+        this.nav = document.querySelector(obj.nav);
+        this.contacts = document.querySelector(obj.contacts);
+        this.init()
+    }
+    init() {
+        this.bindings();
+        this.button.addEventListener("click", this.burger_click);
+    }
+
+    bindings() {
+        [
+            "burger_click",
+            "reveal",
+            "close",
+        ].forEach((method) => {
+            this[method] = this[method].bind(this);
+        });
+    }
+
+    burger_click() {
+        let i = this.button.classList.toggle("header__burger--close");
+        console.log(i);
+        if (i) {
+            this.reveal();
+        }
+        else {
+            this.close();
+        }
+    }
+
+    reveal() {
+        let tl = gsap.timeline();
+        tl.to(this.screen, {
+            onStart: () => {
+                this.screen.style.display = "flex";
+            },
+            opacity: 1,
+            duration: .5,
+        });
+        tl.to(this.nav, {
+            onStart: () => {
+                this.nav.style.display = "block";
+            },
+            opacity: 1,
+            duration: .3,
+            ease: "power1.in",
+        });
+        tl.to(this.contacts, {
+            opacity: 1,
+            duration: .3,
+            ease: "power1.in",
+        }, "<")
+    }
+
+    close() {
+        let tl = gsap.timeline();
+        tl.to(this.contacts, {
+            opacity: 0,
+            duration: .5,
+            ease: "power1.out",
+        })
+        tl.to(this.nav, {
+            onComplete: () => {
+                this.nav.style.display = "none";
+            },
+            opacity: 0,
+            duration: .5,
+            ease: "power1.out",
+        }, "<");
+        tl.to(this.screen, {
+            onComplete: () => {
+                this.screen.style.display = "none";
+            },
+            opacity: 0,
+            duration: .5,
+        });
+    }
+
+}
+
+const menu = new Menu({
+    button: ".header__burger",
+    screen: ".header__screen",
+    nav: ".header__nav",
+    contacts: ".header__contacts",
+})
